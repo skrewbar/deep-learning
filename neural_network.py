3d50a6812d059aae5160c5bdf2fcec8f9167af93
skuru <skurua1244@gmail.com>
2025-03-20 21:28:55 +0900

    🎨 pylint

--- neural_network.py
+++ neural_network.py
@@ -1,29 +1,32 @@
-import numpy as np
 from typing import Self
+import numpy as np
 
 
-def leaky_relu(n: float) -> float:
-    return n if n > 0 else 0.01 * n
+def leaky_relu(n: np.ndarray) -> np.ndarray:
+    """leaky_relu 함수"""
+    return np.where(n > 0, n, 0.01 * n)
 
 
-def leaky_relu_grad(n: float) -> float:
-    return 1 if n > 0 else 0.01
-
-
-vectorized_leaky_relu = np.vectorize(leaky_relu)
-vectorized_leaky_relu_grad = np.vectorize(leaky_relu_grad)
+def leaky_relu_grad(n: np.ndarray) -> np.ndarray:
+    """leaky_relu의 도함수"""
+    return np.where(n > 0, 1, 0.01)
 
 
 class InputLayer:
+    """입력층"""
+
     def __init__(self, cnt: int):
         self.cnt = cnt
         self.activations = np.zeros((1, cnt))
 
     def forward_propagation(self):
+        """순전파 활성도 반환"""
         return self.activations
 
 
 class Layer:
+    """뉴런 층"""
+
     def __init__(self, cnt: int, learning_rate: float, child_layer: Self | InputLayer):
         self.cnt = cnt
         self.lr = learning_rate
@@ -38,14 +41,17 @@ class Layer:
         self.activation_grad = np.zeros((1, cnt))
 
         self.weighted_sum: np.ndarray
+        self.prev_activation: np.ndarray
 
     def forward_propagation(self) -> np.ndarray:
+        """순전파 계산"""
         self.prev_activation = self.child_layer.forward_propagation()
         self.weighted_sum = self.prev_activation.dot(self.weight) + self.bias.T
-        return vectorized_leaky_relu(self.weighted_sum)
+        return leaky_relu(self.weighted_sum)
 
     def back_propagation(self, activation_grad: np.ndarray):
-        bias_grad = vectorized_leaky_relu_grad(self.weighted_sum).T * activation_grad.T
+        """오차역전파"""
+        bias_grad = leaky_relu_grad(self.weighted_sum).T * activation_grad.T
         weight_grad = self.prev_activation.T.dot(bias_grad.T)
 
         self.bias -= self.lr * bias_grad
--- xor.py
+++ xor.py
@@ -1,19 +1,19 @@
-from neural_network import Layer, InputLayer
 import numpy as np
+from neural_network import Layer, InputLayer
 
-lr = 0.005
-epochs = 20000
+LR = 0.005
+EPOCHS = 20000
 
 input_layer = InputLayer(2)
-hidden_layer = Layer(4, lr, input_layer)
-output_layer = Layer(1, lr, hidden_layer)
+hidden_layer = Layer(4, LR, input_layer)
+output_layer = Layer(1, LR, hidden_layer)
 
 inputs = list(
     map(lambda x: np.array(x).reshape(1, 2), [[0, 0], [0, 1], [1, 0], [1, 1]])
 )
 answers = [0, 1, 1, 0]
 
-for i in range(1, epochs + 1):
+for i in range(1, EPOCHS + 1):
     for k in range(4):
         input_layer.activations = inputs[k]
         output = output_layer.forward_propagation()
