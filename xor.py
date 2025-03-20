7486dbc7e18c036c1fae4123aa8a471f1892b890
skuru <skurua1244@gmail.com>
2025-03-20 21:17:28 +0900

    ⚡️ 은닉층 늘려서 정확도 상승

--- xor.py
+++ xor.py
@@ -1,29 +1,25 @@
-from neural_network import Layer, InputLayer, Neuron
+from neural_network import Layer, InputLayer
 import numpy as np
 
-lr = 0.01
+lr = 0.005
 epochs = 20000
 
 input_layer = InputLayer(2)
-hidden_layer = Layer(2, lr, input_layer)
+hidden_layer = Layer(4, lr, input_layer)
 output_layer = Layer(1, lr, hidden_layer)
 
-# input_layer.set_activations(np.array([1, 1]))
-# answer = np.array([0])
-# output = output_layer.get_activations()
-# output_layer.learn((answer - output) ** 2)
-
-
-inputs = list(map(lambda x: np.array(x), [[0, 0], [0, 1], [1, 0], [1, 1]]))
+inputs = list(
+    map(lambda x: np.array(x).reshape(1, 2), [[0, 0], [0, 1], [1, 0], [1, 1]])
+)
 answers = [0, 1, 1, 0]
 
 for i in range(1, epochs + 1):
     for k in range(4):
-        input_layer.set_activations(inputs[k])
-        output = output_layer.get_activations()
+        input_layer.activations = inputs[k]
+        output = output_layer.forward_propagation()
         error = sum((output - answers[k]) ** 2)
-        error_der = 2 * (output - answers[k])
-        output_layer.learn(error_der)
+        error_grad = 2 * (output - answers[k])
+        output_layer.back_propagation(error_grad)
 
-        if i % 10000 == 0:
+        if i % 5000 == 0:
             print(f"{i}째 | 출력: {output}, 답: {answers[k]}, 오차: {error}")
